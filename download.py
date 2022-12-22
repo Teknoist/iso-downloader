6bd10c0e01af10504a84f10d55077de0bb89298f
Teknoist <37031361+Teknoist@users.noreply.github.com>
2022-12-22 20:53:43 +0300

    Create download.py

---
+++ download.py
@@ -0,0 +1,29 @@
+import os
+import re
+import xml.etree.ElementTree as ET
+import urllib.request
+
+# 1. Komut satırı argümanlarını okuyun
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--distro', required=True)
+args = parser.parse_args()
+
+# 2. repo.xml dosyasını indirin ve ağaç yapısına dönüştürün
+url = 'https://example.com/repo.xml'
+xml_data = urllib.request.urlopen(url).read()
+root = ET.fromstring(xml_data)
+
+# 3. Belirtilen dağıtımlar için güncel sürümleri indirin
+distros = args.distro.split(',')
+for distro in distros:
+    file_name = f'{distro}.iso'
+    package = root.find(f"package[name='{file_name}']")
+    if package is not None:
+        version = package.find('version').text
+        download_url = package.find('download_url').text
+        file_path = f'./download/{file_name}'
+        urllib.request.urlretrieve(download_url, file_path)
+        print(f'{file_name} version {version} has been downloaded.')
+    else:
+        print(f'{file_name} not found in the repository.')
