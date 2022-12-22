9eb1589f781b498d59d2ee4fae8333f3d572c0ab
Teknoist <37031361+Teknoist@users.noreply.github.com>
2022-12-22 18:11:37 +0300

    Update isoget.ps1

--- isoget.ps1
+++ isoget.ps1
@@ -2,16 +2,21 @@ Install-Module -Name Invoke-WebRequest -Force
 Install-Module -Name XML -Force
 
 param (
-   [string]$ISOFileName,
-   [string]$Version
+   [string]$install,
+   [string]$Ver
 )
 
+# Repository dosyasını indirin
+$RepoUrl = "http://example.com/repository.xml"
+$LocalRepoFile = ".\repo.xml"
+Invoke-WebRequest -Uri $RepoUrl -OutFile $LocalRepoFile
+
 # Repository dosyasını yükleyin ve içeriğini okuyun
-$RepoFile = "C:\Repositories\repository.xml"
+$RepoFile = ".\repo.xml"
 $RepoContent = Get-Content $RepoFile
 
 # ISO dosyasını arayın ve linkini alın
-$ISO = Select-Xml -Content $RepoContent -XPath "//iso[name='$ISOFileName' and version='$Version']"
+$ISO = Select-Xml -Content $RepoContent -XPath "//iso[name='$install' and version='$Ver']"
 $ISOLink = $ISO.Node.link.InnerText
 
 # ISO dosyasını indirin
